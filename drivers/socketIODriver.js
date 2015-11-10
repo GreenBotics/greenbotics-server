f9763978cb9b08d9f694dc1cdd8f3aed28bc76ba
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-11-10 20:17:47 +0100

    feature(socketIODriver): added VERY basic outbound message dispatching

--- drivers/socketIODriver.js
+++ drivers/socketIODriver.js
@@ -6,9 +6,9 @@ const fromEvent = Rx.Observable.fromEvent
 export default function makeSocketIODriver (app){
   let io = sio(app)
 
-  function SocketIODriver(){
-    let connection$ = fromEvent(  io.sockets, 'connection' )
-    connection$.forEach(socket=>console.log("connection made to socket"))
+  function SocketIODriver(outgoing$){
+    let connections$ = fromEvent(  io.sockets, 'connection' )
+    connections$.forEach(socket=>console.log("connection made to socket by",socket.id))
 
     /*let someEvent$ = connection$
       .flatMap(socket=>fromEvent('someEvent',socket))
@@ -19,12 +19,42 @@ export default function makeSocketIODriver (app){
     //let socketIn$ = connection$ 
     //connection$.subscribe(e=>socket$.onNext(e))
     //let socketOut$ = new Rx.Subject()
-    connection$.forEach(socket=>socket.emit("bla"))
+    //connection$.forEach(socket=>console.log("sockets",socket))
+
+    function publish({socket, messageType, message}) {
+      console.log("publish to",socket.id,messageType,message)
+      socket.emit(messageType, message);
+    }
+
+    outgoing$
+      .withLatestFrom(connections$,function(outgoing,socket){
+        const {messageType,message} = outgoing
+        return {socket, messageType, message}
+      })
+      .forEach(publish)
 
     function get(eventName){
-      return connection$
+      return connections$
         .flatMap(socket=>fromEvent(socket, eventName))
     }
+
+    /*var _emit = socket.emit
+    _onevent = socket.onevent
+
+
+    //attempt to override to enable filtering
+    socket.emit = function () { //Override outgoing
+        //Do your logic here
+        console.log('***', 'emit', arguments)
+        _emit.apply(socket, arguments)
+    };
+
+    socket.onevent = function (packet) { //Override incoming
+        var args = packet.data || []
+        //Do your logic here
+        console.log('***', 'onevent', packet)
+        _onevent.call(socket, packet)
+    }*/
 
     return {
       get
