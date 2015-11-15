4ece96979fb0a7d6e9eb2e5e11e275482b464cc7
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-11-15 14:08:14 +0100

    refactor(server):restructured & cleaned up main server module: - split out various db/http/socket io elements into different functions - refactored & cleaned up http request handling - added basics of potential "intent"/model

--- index.js
+++ index.js
@@ -17,108 +17,118 @@ import {formatData} from './utils/sensorUtils'
 import {combineLatestObj} from './utils/obsUtils'
 
 
-function baseData({socketIO, db}){
+function intent(drivers){
+  const {socketIO, http} = drivers
+
   socketIO.get('initialData')
     .forEach(e=>console.log("initialData request",e))
 
-  const nodes$ = socketIO.get('initialData')
+  const getInitialData$ = socketIO.get('initialData')
     .flatMap( e=>db.find("nodes",{},{toArray:true}) )
 
-    //.startWith([])
+  
+  return {
+    getInitialData$
+  }
+}
 
-  nodes$
-    .forEach(e=>console.log("nodes",e))
+function model(actions){
+  const nodes$ = actions.getInitialData$
+  const feeds$ = undefined
 
   const data$ = combineLatestObj({nodes$})
 
-  //data$
-  //  .filter(e=>e!==undefined)
-    //.forEach(e=>console.log("baseData",e.nodes[0]))
-  return nodes$
+  return nodes$  
 }
 
 
-function main(drivers) {
-  const {socketIO, db, http} = drivers
-  
-  //const actions = intent(drivers)
-  //const state$  = model(undefined, actions, drivers)
 
-  const sensorJobTimer$ = cronJob('*/50 * 3 * * *')
-    .stream
+function socketIO(state$){
+  const outgoingMessages$ = state$.map( 
+    function(eventData){
+      return {
+        messageType: 'initialData',
+        message: JSON.stringify(eventData)
+      }
+  })
 
-  //db
-  /*db.find("node1SensorData",{}).forEach(e=>console.log("found",e))
-  db.find("node1SensorData",{temperature: { $gt: 21.82 } },{toArray:true})
-    .forEach(e=>console.log("found",e))*/
+  return outgoingMessages$
+}
 
-  //http requests
-  
-  let node0Data$ = http
-    .filter(res$ => res$.request.name === 'node0')
-    .mergeAll()
-    .pluck("response")
-    .pluck("variables")
-    .map(formatData)
-  
-  let node1Data$ = http
-    .filter(res$ => res$.request.name === 'node1')
-    .mergeAll()
-    .pluck("response")
-    .pluck("variables")
-    .map(formatData)
-
-  node0Data$.forEach(e=>console.log("node0Data recieved",e))
-  node1Data$.forEach(e=>console.log("node1Data recieved",e))
-
-
+function requests(drivers, sensorJobTimer$){
   //outbound requests
-  let node0Reqs$ = sensorJobTimer$
+  const node0Reqs$ = sensorJobTimer$
+    .do(e=>console.log("fetch node0 sensor data"))
     .map(e=> ({
           url: "http://192.168.1.20:3020"
           , method: 'get'
           , responseType:"json"
           , name: 'node0'
-          , anyOtherDataYouWish: 'foo'
+          , type: 'feedData'
         })
     )
 
-  let node1Reqs$ = sensorJobTimer$
+  const node1Reqs$ = sensorJobTimer$
+    .do(e=>console.log("fetch node1 sensor data"))
     .map(e=> ({
           url: "http://192.168.1.21:3020"
           , method: 'get'
           , responseType:"json"
           , name: 'node1'
-          , anyOtherDataYouWish: 'foo'
+          , type: 'feedData'
         })
     )
 
+  function fetchSensorFeeds(nodes){
+    return nodes
+      .map(function(node){
+
+      })
+  }
+
+  //const requests$ = sensorJobTimer$.flatMap
   const requests$ = Rx.Observable.merge(node0Reqs$,node1Reqs$)
+  return requests$
+}
+
+function db(drivers){
+  const {http} = drivers
+
+  const sensorFeedsData$ = http
+    .filter(res$ => res$.request.type === 'feedData')
     .retry(3)
-  
-  //return anything you want to output to drivers
-
-  /*const baseClientData$ = socketIO.get('initialData')
-    .forEach(e=>console.log("initialData",e))*/
-
-
-  const incomingMessages$ = socketIO.get('someEvent')
-    .map(e=>JSON.parse(e))
-  
-
-  //incomingMessages$
-  const node0DbOut$ = node0Data$
-    .map(function(e){
-      return {collectionName:"node0SensorData", data:e}
+    .catch(function(e){
+      console.log("ouch , problem fetching data for node1",e)
+      return Rx.Observable.empty()
     })
-
-  const node1DbOut$ = node1Data$
-    .map(function(e){
-      return {collectionName:"node1SensorData", data:e}
+    .flatMap(function(e){
+      const request  = Rx.Observable.just(e.request)
+      const response = e.pluck("response")
+      return combineLatestObj({response,request})
     })
+    .map(function(e){
+      const data           = e.response.variables
+      const collectionName = e.request.name+"SensorData"
+      return {collectionName, data}
+    })
+    /*.mergeAll()
+    .catch(function(e){
+      //console.log("ouch , problem fetching data for node0",e)
+      return Rx.Observable.empty()
+    })
+    .pluck("response")
+    .pluck("variables")
+    .map(formatData)*/
+    //.do(e=>console.log("sensorFeedsData",e))
 
+  return sensorFeedsData$
+}
 
-  const nodes = [
+  //db
+  /*db.find("node1SensorData",{}).forEach(e=>console.log("found",e))
+  db.find("node1SensorData",{temperature: { $gt: 21.82 } },{toArray:true})
+    .forEach(e=>console.log("found",e))*/
+/*const nodes = [
     { uid:"1b49763e-8aad-4c2b-8326-46b7548c232b"
       ,name:"Weather station"
       ,uri:"http://192.168.1.20:3020"
@@ -145,36 +155,24 @@ function main(drivers) {
         ,{feedId:2, type:"pressure" }
       ]
     }
-  ]
+  ]*/
 
-  const dbOutput$ = Rx.Observable.merge(node0DbOut$,node1DbOut$)
-    /*Rx.Observable.just(nodes)
-    .map(function(e){
-      return {collectionName:"nodes",data:e}
-    })*/
+
+function main(drivers) {  
+  const actions = intent(drivers)
+  const state$  = model(actions, drivers)
+
+  const sensorJobTimer$ = cronJob('*/10 * * * * *')
+    .stream
+  
+  const requests$ = requests(drivers, sensorJobTimer$)
+  const db$       = db(drivers)
+  const sIO$      = socketIO(state$)
+
  
-  //outputs 
-
-
-  /*const outgoingMessages$ = Rx.Observable
-    .interval(500)
-    .map(function(e){
-      return {messageType:"foo",message:"bar"}
-    })*/
-
-  const outgoingMessages$ = baseData(drivers).map( 
-    function(eventData){
-      return {
-        messageType: 'initialData',
-        message: JSON.stringify(eventData)
-      }
-    })
-    .merge(Rx.Observable.just({messageType:"foo",message:'bar'}))
-
-
   return {
-    socketIO: outgoingMessages$
-    ,db: dbOutput$
+    socketIO: sIO$
+    ,db: db$
     ,http:requests$
   }
 }
