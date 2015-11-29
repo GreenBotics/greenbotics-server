b4cf043c1dca7421024750afadfb8275ebc7eebc
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-11-29 20:49:09 +0100

    refactor(feeds): cleaned up & extracted various functions used to fetch feed data

--- nodes/feeds.js
+++ nodes/feeds.js
@@ -1,20 +1,21 @@
-import {flatten,mergeAll} from 'ramda'
+import {flatten,concat} from 'ramda'
 import Rx from 'rx'
 
-export function getFeedsData(drivers, searchCriteria, limit=10){
-  const {db} = drivers
-  console.log("I am now getting feeds data",searchCriteria)
+
+function dbExperiments(){
   //db.find("nodes",{feedId:4,_id:2},{toArray:true}).forEach(e=>console.log("found nodes",e))
   //db.find("nodes",{feedId:{$all:[4]}},{toArray:true}).forEach(e=>console.log("found nodes2",e))
-
   const crit = {$in:[2,3]}//[2,3]
   const _id = {_id:crit}
   //db.find("nodes",_id,{toArray:true}).forEach(e=>console.log("found nodes3",e))
   //db.find("nodes",{$all:[0]},{toArray:true}).forEach(e=>console.log("found nodes0",e))
   //db.find("node0SensorData",{},{0:1, _id:0,"timestamp":1},{toArray:true}).forEach(e=>console.log("found nodes1",e))
+}
 
 
-  //alternative
+function extractFeedsData(drivers,searchCriteria,limit){
+  const {db} = drivers
+
   let feedFields = searchCriteria.reduce(function(acc,criteria){
 
     if(!acc[criteria.node]){
@@ -27,8 +28,8 @@ export function getFeedsData(drivers, searchCriteria, limit=10){
   console.log("feedFields", feedFields)
 
   const collectionNames = {
-    2:"node0SensorData"
-    ,3:"node1SensorData"
+    0:"node0SensorData"
+    ,2:"node1SensorData"
   }
 
   const nodes = Object.keys(feedFields)
@@ -41,16 +42,22 @@ export function getFeedsData(drivers, searchCriteria, limit=10){
 
     const result$ = db.find(collectionName, {}, projection, {toArray:true})
       .map( data=>data.slice(0,30) )
-      .map( data=> {
+      /*.map( data=> {
         let out= {}
         out[node] = data
         return out
-      })
+      })*/
     return result$.take(1)
   })
 
-  //hack for now   
-  /*const results$ = searchCriteria.map(function(criteria){
+  return Rx.Observable.forkJoin(results$)//.map(data=>flatten(data))
+    .map(flatten)
+
+}
+
+
+function extractFeedsData_variant2(drivers,searchCriteria,limit){
+  const results$ = searchCriteria.map(function(criteria){
     //console.log("mapping searchCriteria",criteria)
     let collectionName = undefined
     if(criteria.node === 2){
@@ -67,9 +74,13 @@ export function getFeedsData(drivers, searchCriteria, limit=10){
     const result$ = db.find(collectionName, {}, feedFields, {toArray:true})
       .map( data=>data.slice(0,30) )
     return result$.take(1)
-  })*/
+  })
+}
 
-  const out$ = Rx.Observable.forkJoin(results$)//.map(data=>flatten(data))
-    .map( resuls => mergeAll(results) ) 
+
+export function getFeedsData(drivers, searchCriteria, limit=10){
+  console.log("I am now getting feeds data",searchCriteria)
+
+  const out$ = extractFeedsData(drivers,searchCriteria,limit)
   return out$
 }
