a02cf4ba7adbf10d295280db4a053a11ffa67672
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-02-12 00:20:29 +0100

    feature(helpers): added utility functions to handle action loading/creation - also for merging actions by name

--- utils/obsUtils.js
+++ utils/obsUtils.js
@@ -1,4 +1,6 @@
 import Rx from 'rx'
+const {merge} = Rx.Observable
+import {flatten} from 'Ramda'
 
 /*From https://github.com/staltz/combineLatestObj*/
 export function combineLatestObj(obj) {
@@ -18,4 +20,96 @@ export function combineLatestObj(obj) {
     }
     return combination;
   })
+}
+
+
+
+/*merges an array of action objects into a single object :ie
+ [{doBar$, doFoo$}, {doBaz$, doBar$}] => {doBar$, doFoo$, doBaz$} 
+*/
+export function mergeActionsByName(actionSources, validActions=[]){
+
+  return actionSources.reduce(function(result, actions){
+    //console.log("acions",Object.keys(actions),validActions)
+    Object.keys(actions)
+      .filter(key=> validActions.length === 0 || validActions.indexOf(key.replace('$',''))>-1)
+      .map(function(key){
+        const action = actions[key]
+        if(key in result){
+          result[key] = merge(result[key], action)
+        }else{
+          result[key] = action
+        }       
+      })
+
+    return result
+  },{})
+ 
+}
+
+
+
+/*utility function to dynamically load and use the "data extractors" (ie functions that
+ extract useful data from raw data)
+*/
+export function actionsFromSources(sources, basePath){
+
+  const data = Object.keys(sources).map(function(sourceName){
+    try{
+      const modulePath     = basePath+sourceName
+      const actionsImport  = require(modulePath)
+      //const sourceData     = sources[sourceName]//the raw source of data (ususually a driver)
+
+      return actionsImport.intent(sources)
+    }catch(error){}
+  })
+  .filter(data=>data!==undefined)
+
+  return mergeActionsByName( data )
+  //return merge( flatten( data ) )
+}
+
+
+/*utility function to dynamically load and use the "data extractors" (ie functions that
+ extract useful data from raw data)
+*/
+export function extractDataFromRawSources(sources, basePath){
+
+  const data = Object.keys(sources).map(function(sourceName){
+    try{
+      const extractorImport = require('../../core/sources/'+sourceName)
+      
+      const sourceData     = sources[sourceName]//the raw source of data (ususually a driver)
+      const extractorNames = Object.keys(extractorImport)
+
+
+
+      //TODO , find a better way to do this
+      const paramsHelper = {
+        get:function get(category, params){
+          const data = {
+            'extensions':{ foo: []}
+          }
+          return data[category][params]
+        }
+      }
+      
+      //deal with all the different data "field" functions that are provided by the imports
+      const refinedData =  extractorNames.map(function(name){
+        const fn = extractorImport[name]
+        if(fn){
+          const refinedData = fn(sourceData, paramsHelper)
+            .flatMap(fromArray)
+            .filter(exists)
+            return refinedData
+        }
+      })
+
+      return refinedData
+     
+    }catch(error){}
+  })
+  .filter(data=>data!==undefined)
+
+  return merge( flatten( data ) )
 }
