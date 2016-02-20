99e34b15a0d3d7527119ae86238149007e82d5bc
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-02-20 14:20:27 +0100

    refactor(fromSocketId): cleanup of fromSocketIo module

--- utils/fromSocketIo.js
+++ utils/fromSocketIo.js
@@ -1,37 +1,37 @@
 export function socketIO(address, openObserver) {
-    let socket = io(address)
-    // Handle the data
-    let observable = Rx.Observable.create (function (obs) {
-        if (openObserver) {
-            socket.on('connect', function () {
-                openObserver.onNext()
-                openObserver.onCompleted()
-            })
-        }
+  let socket = io(address)
+  // Handle the data
+  let observable = Rx.Observable.create (function (obs) {
+      if (openObserver) {
+          socket.on('connect', function () {
+              openObserver.onNext()
+              openObserver.onCompleted()
+          })
+      }
 
-        // Handle messages
-        socket.io.on('packet', function (packet) {
-            if (packet.data) obs.onNext({
-                name: packet.data[0],
-                data: packet.data[1]
-            })
-        })
-        socket.on('error', function (err) { obs.onError(err) })
-        socket.on('reconnect_error', function (err) { obs.onError(err) })
-        socket.on('reconnect_failed', function () { obs.onError(new Error('reconnection failed')) })
-        socket.io.on('close', function () { obs.onCompleted() })
+      // Handle messages
+      socket.io.on('packet', function (packet) {
+          if (packet.data) obs.onNext({
+              name: packet.data[0],
+              data: packet.data[1]
+          })
+      })
+      socket.on('error', function (err) { obs.onError(err) })
+      socket.on('reconnect_error', function (err) { obs.onError(err) })
+      socket.on('reconnect_failed', function () { obs.onError(new Error('reconnection failed')) })
+      socket.io.on('close', function () { obs.onCompleted() })
 
-        // Return way to unsubscribe
-        return function() {
-            socket.close()
-        }
-    })
+      // Return way to unsubscribe
+      return function() {
+          socket.close()
+      }
+  })
 
-    let observer = Rx.Observer.create(function (event) {
-        if (socket.connected) {
-            socket.emit(event.name, event.data)
-        }
-    })
+  let observer = Rx.Observer.create(function (event) {
+      if (socket.connected) {
+          socket.emit(event.name, event.data)
+      }
+  })
 
-    return Rx.Subject.create(observer, observable)
+  return Rx.Subject.create(observer, observable)
 }
